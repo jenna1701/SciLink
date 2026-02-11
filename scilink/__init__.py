fa1bdd5eafc8b89eecde2272c65d7b213bcb82b6
Maxim Ziatdinov <maxim.ziatdinov@pnnl.gov>
2026-02-11 08:38:58 -0800

    Make simulation packages (ase, aimsgb, atomate2, mdanalysis) optional under [sim] extra

--- scilink/__init__.py
+++ scilink/__init__.py
@@ -33,19 +33,9 @@ def show_config():
     """Show current API key configuration status"""
     show_api_status()
 
-from .workflows import ExperimentNoveltyAssessment
-from .workflows.dft_workflow import DFTWorkflow
-from .workflows.dft_recommendation_workflow import DFTRecommendationsWorkflow
-from .workflows.experiment2dft import Experimental2DFT
 
 __all__ = [
     'configure', 
     'configure_from_dict', 
-    'show_config',
-    'MicroscopyNoveltyAssessmentWorkflow',
-    'DFTWorkflow', 
-    'SpectroscopyNoveltyAssessmentWorkflow',
-    'DFTRecommendationsWorkflow',
-    'Experimental2DFT',
-    'ExperimentNoveltyAssessment'
+    'show_config'
 ]
